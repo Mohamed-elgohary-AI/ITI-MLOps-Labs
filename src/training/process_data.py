2bf8cf1081330e56e467c84db5064b3ce71c123f
Mohamed Elgohary <mohamedabdelmonemelgohary@gmail.com>
2026-04-28 08:44:42 +0000

    add configuration pipeline using hydra and new rf model

--- src/training/process_data.py
+++ src/training/process_data.py
@@ -7,25 +7,24 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from omegaconf import DictConfig
 
 SOURCE = os.path.join("data", "raw")
 DESTINATION = os.path.join("data", "processed")
 
 
 def read_process_data(
-    file_name: str,
-    id_col: str,
-    target_col: str,
+    cfg: DictConfig,
     logger,
 ) -> None:
     logger.info("Data Processing started")
-    df = pd.read_csv(os.path.join(SOURCE, f"{file_name}.csv"))
-    df.set_index(id_col, inplace=True)
+    df = pd.read_csv(os.path.join(SOURCE, f"{cfg.data.file_name}.csv"))
+    df.set_index(cfg.data.id_col, inplace=True)
     train_df, test_df = train_test_split(
-        df, test_size=0.15, random_state=42, stratify=df[target_col]
+        df, test_size=0.15, random_state=42, stratify=df[cfg.data.target_col]
     )
-    train_df.to_parquet(os.path.join(DESTINATION, f"{file_name}-train.parquet"), engine="pyarrow")
-    test_df.to_parquet(os.path.join(DESTINATION, f"{file_name}-test.parquet"), engine="pyarrow")
+    train_df.to_parquet(os.path.join(DESTINATION, f"{cfg.data.file_name}-train.parquet"), engine="pyarrow")
+    test_df.to_parquet(os.path.join(DESTINATION, f"{cfg.data.file_name}-test.parquet"), engine="pyarrow")
 
 
 def preprocess_data(
--- trainer.py
+++ trainer.py
@@ -1,22 +1,24 @@
 from src.logger import ExecutorLogger
-from src.training.download_data import download_iris_data
+from src.training.download_data import download_data
 from src.training.evaluate import evaluate
 from src.training.process_data import read_process_data, preprocess_data
 from src.training.train import encode_target_col, trainer
+import hydra
+from omegaconf import DictConfig
 
-
-def main(logger) -> None:
+@hydra.main(config_path="conf", config_name="config", version_base=None)
+def main(cfg: DictConfig) -> None:
+    logger = ExecutorLogger("training")
     logger.info("Training started")
-    download_iris_data(logger)
-    read_process_data("train", "PassengerId", "Survived", logger)
-    X, y, X_test, y_test = encode_target_col("train", "Survived", "fake", logger)
+    download_data(logger)
+    read_process_data(cfg, logger)
+    X, y, X_test, y_test = encode_target_col(cfg, logger)
     X_processed, X_test_procesed=preprocess_data(X, y, X_test, y_test, logger)
-    trainer(X_processed, y, "lr","xgb", logger)
-    evaluate(X_test_procesed, y_test, "lr", logger)
-    evaluate(X_test_procesed, y_test, "xgb", logger)
+    trainer(X_processed, y, cfg,logger)
     logger.info("Training finished")
+    evaluate(X_test_procesed, y_test,cfg,logger)
+    logger.info("Evaluation Finished")
 
 
 if __name__ == "__main__":
-    logger = ExecutorLogger("training")
-    main(logger)
+    main()
